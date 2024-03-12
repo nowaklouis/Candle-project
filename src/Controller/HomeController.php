f8804e48d64177c50bb7971bd59887f4fa2de4da
louis nowak <nowak.louis@gmail.com>
2024-03-12 15:50:12 +0100

    home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $entity
+    ) {
+    }
+
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $products = $this->entity->getRepository(Product::class)->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $products,
         ]);
     }
 }
