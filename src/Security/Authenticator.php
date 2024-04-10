e7cfda07a25542fb0833699481a17efe441a67c9
louis nowak <nowak.louis@gmail.com>
2024-04-10 18:43:46 +0200

    home/css in progress

--- src/Security/Authenticator.php
+++ src/Security/Authenticator.php
@@ -48,8 +48,7 @@ class Authenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
         throw new \Exception('TODO: provide a valid redirect inside ' . __FILE__);
     }
 
